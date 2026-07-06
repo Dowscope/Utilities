daf86ff5d43b682b01529654f469910e7f493c95
Dowscope <timothy.dowling@me.com>
2026-07-06 11:21:53 -0400

    Updated the configuration of nginx

--- Linux/Debian/lib/nginx.sh
+++ Linux/Debian/lib/nginx.sh
@@ -6,12 +6,33 @@ NGINX_PACKAGES=(
     nginx
 )
 
+NGINX_CONFIG_URL="$REPO_BASE/config/nginx/default"
+NGINX_AVAILABLE="/etc/nginx/sites-available/default"
+NGINX_ENABLED="/etc/nginx/sites-enabled/default"
+
+########################################
+# Configure
+########################################
+
+configure_nginx() {
+    log "Configuring Nginx"
+
+    curl -fsSL "$NGINX_CONFIG_URL" -o "$RUN_TMP/default"
+
+    run cp "$RUN_TMP/default" "$NGINX_AVAILABLE"
+    run ln -sfn "$NGINX_AVAILABLE" "$NGINX_ENABLED"
+
+    run nginx -t
+}
+
 ########################################
 # Install
 ########################################
 
 install_nginx() {
     install_packages "Nginx" "${NGINX_PACKAGES[@]}"
+
+    configure_nginx
 
     run systemctl enable nginx
     run systemctl restart nginx
@@ -30,4 +51,3 @@ remove_nginx() {
     run rm -rf /etc/nginx
     run rm -rf /var/www/html
 }
-
