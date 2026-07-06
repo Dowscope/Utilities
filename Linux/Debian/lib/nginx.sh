05f7d85fb11eb65541aaa1c30e91b2d424b08ef9
Dowscope <timothy.dowling@me.com>
2026-07-06 11:27:34 -0400

    Update nginx files

--- Linux/Debian/lib/nginx.sh
+++ Linux/Debian/lib/nginx.sh
@@ -16,9 +16,11 @@ NGINX_ENABLED="/etc/nginx/sites-enabled/default"
 
 configure_nginx() {
     log "Configuring Nginx"
-
+    
+    echo "Downloading NGINX configuration file"
     curl -fsSL "$NGINX_CONFIG_URL" -o "$RUN_TMP/default"
 
+    echo "Replacing and linking new configuartion file"
     run cp "$RUN_TMP/default" "$NGINX_AVAILABLE"
     run ln -sfn "$NGINX_AVAILABLE" "$NGINX_ENABLED"
 
@@ -43,11 +45,14 @@ install_nginx() {
 ########################################
 
 remove_nginx() {
+    echo "Stopping and disabling NGINX"
     run systemctl stop nginx || true
     run systemctl disable nginx || true
 
+    echo "Removing packages"
     remove_packages "Nginx" "${NGINX_PACKAGES[@]}"
 
+    echo "Removing nginx files and website files"
     run rm -rf /etc/nginx
     run rm -rf /var/www/html
 }
